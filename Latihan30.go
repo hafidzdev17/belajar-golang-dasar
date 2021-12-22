f5d9d23a44b8ba77c45947e05e625adfd915af73
hafet17 <do.crazy192@gmail.com>
2021-12-22 14:11:41 +0700

    add: Latihan30 (Package Ring)

---
+++ Latihan30.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"strconv"
+)
+
+// TODO: Package Ring
+
+func main() {
+
+	var data *ring.Ring = ring.New(5)
+
+	for i := 0; i < data.Len(); i++ {
+		data.Value = "Data " + strconv.FormatInt(int64(i), 10)
+		data = data.Next()
+	}
+
+	data.Do(func(value interface{}) {
+		fmt.Println(value)
+	})
+}
